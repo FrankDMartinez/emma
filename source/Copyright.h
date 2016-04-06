18bfb91f275ced4e9592746bde8638ff8efc1201
FrankDMartinez <Frank@tests-Mac-mini.local>
2016-04-06 15:35:12 -0500

    Cleaning up some spacing issues

--- source/Copyright.h
+++ source/Copyright.h
@@ -1,11 +1,13 @@
 //===--- Copyright.h - Copyright Related Functionality ---===//
 //
-// This file is part of the Election Method Mathematics Application (EMMA) project.
+// This file is part of the Election Method Mathematics
+// Application (EMMA) project.
 //
 // Copyright (c) 2015 - 2016 Frank D. Martinez and the EMMA project authors
 // Licensed under the GNU Affero General Public License, version 3.0.
 //
-// See the file called "LICENSE" included with this distribution for license information.
+// See the file called "LICENSE" included with this distribution
+// for license information.
 //
 //===----------------------------------------------------------------------===//
 //
